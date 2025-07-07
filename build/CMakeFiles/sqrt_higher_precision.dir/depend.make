feb605896d0e78fd4b5247b1250f2a05589d76fe
Abega1642 <hei.abega.2@gmail.com>
2025-07-07 21:03:09 +0300

    feat: echo 'build' >> .gitignore

--- build/CMakeFiles/sqrt_higher_precision.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for sqrt_higher_precision.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/abega/Documents/Computer_Science_works/sqrt-higher-precision
-# Build directory: /home/abega/Documents/Computer_Science_works/sqrt-higher-precision/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[all_tests]=] "/home/abega/Documents/Computer_Science_works/sqrt-higher-precision/build/tests")
-set_tests_properties([=[all_tests]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/abega/Documents/Computer_Science_works/sqrt-higher-precision/CMakeLists.txt;40;add_test;/home/abega/Documents/Computer_Science_works/sqrt-higher-precision/CMakeLists.txt;0;")
