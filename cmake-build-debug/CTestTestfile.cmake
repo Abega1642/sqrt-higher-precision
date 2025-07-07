d5284f76be251ccbeaa783d1c24c9091c2a3b108
Abega1642 <hei.abega.2@gmail.com>
2025-07-07 23:54:50 +0300

    feat: echo 'cmakge-build-debug' >> .gitignore

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/abega/Documents/Computer_Science_works/sqrt-higher-precision
-# Build directory: /home/abega/Documents/Computer_Science_works/sqrt-higher-precision/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[all_tests]=] "/home/abega/Documents/Computer_Science_works/sqrt-higher-precision/cmake-build-debug/tests")
-set_tests_properties([=[all_tests]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/abega/Documents/Computer_Science_works/sqrt-higher-precision/CMakeLists.txt;50;add_test;/home/abega/Documents/Computer_Science_works/sqrt-higher-precision/CMakeLists.txt;0;")
-subdirs("_deps/googletest-build")
--- cmake-build-debug/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/abega/Documents/Computer_Science_works/sqrt-higher-precision/cmake-build-debug/_deps/googletest-src/googletest
-# Build directory: /home/abega/Documents/Computer_Science_works/sqrt-higher-precision/cmake-build-debug/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
