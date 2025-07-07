c903bbed898b4c2695d198401f2f0e3352eb4437
Abega1642 <hei.abega.2@gmail.com>
2025-07-07 21:10:20 +0300

    fixup

---
+++ cmake-build-debug/_deps/googletest-build/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/abega/Documents/Computer_Science_works/sqrt-higher-precision/cmake-build-debug/_deps/googletest-src/googlemock
+# Build directory: /home/abega/Documents/Computer_Science_works/sqrt-higher-precision/cmake-build-debug/_deps/googletest-build/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
