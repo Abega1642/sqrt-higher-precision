d8181f77a2a203dc6b6e57bae6ef42adb5a5e241
Abega1642 <hei.abega.2@gmail.com>
2025-07-11 10:47:14 +0300

    test: benchmark test for SquareRoot::k_th_value for (n=2,k=22)

--- tests/test_bench_marks.cpp
+++ tests/test_bench_marks.cpp
@@ -1,11 +1,26 @@
 #include <gtest/gtest.h>
 #include <chrono>
+#include <string>
 #include "../include/SquareRoot.hpp"
 #include "../include/Fraction.hpp"
 
+void show_result(
+	const std::string& title,
+	const unsigned long nat,
+	const unsigned long range,
+	const unsigned long precision_digits,
+	const double time
+) {
+	std::cout << "[Benchmark of SquareRoot::k_th_value]  - " << title
+			  << "\n\t - nat\t\t\t= " << nat
+			  << ",\n\t - k\t\t\t= " << range
+			  << ",\n\t - Precision\t= " << precision_digits << " decimal digits"
+			  << ",\n\t - Duration\t\t= " << time << " seconds"
+	<< std::endl;
+}
 
 /*
-* [Benchmark of SquareRoot::k_th_value]
+* [Benchmark of SquareRoot::k_th_value] - Fraction Form
 	 - nat			= 2,
 	 - k			= 22,
 	 - Precision	= 6000002 decimal digits,
@@ -27,12 +42,35 @@ TEST(SquareRootBenchmark, TimeExecutionOfKthValueComputation) {
 
 	const std::chrono::duration<double> elapsed_seconds = end - start;
 
-	std::cout << "[Benchmark of SquareRoot::k_th_value] \n "
-			  << "\t - nat\t\t\t= " << subject.get_nat()
-	          << ",\n\t - k\t\t\t= " << k
-			  << ",\n\t - Precision\t= " << precision_digits << " decimal digits"
-			  << ",\n\t - Duration\t\t= " << elapsed_seconds.count() << " seconds"
-	<< std::endl;
+	show_result("Fraction form", subject.get_nat(), k, precision_digits, elapsed_seconds.count());
+
+	SUCCEED();
+}
+
+/*
+* [Benchmark of SquareRoot::k_th_value] - Decimal Form
+	 - nat			= 2,
+	 - k			= 22,
+	 - Precision	= 6000002 decimal digits,
+	 - Duration		= 10.2902 seconds
+ */
+
+TEST(SquareRootBenchmark, TimeExecutionOfKthValueComputationWithSixMillionPreision) {
+	constexpr unsigned long target = 2UL;
+	constexpr unsigned long k = 22UL;
+	constexpr std::size_t precision_digits = 6'000'002;
+
+	const SquareRoot subject = SquareRoot::of(target);
+
+	const auto start = std::chrono::high_resolution_clock::now();
+
+	const std::string result = subject.k_th_value(k).get_value(precision_digits);
+
+	const auto end = std::chrono::high_resolution_clock::now();
+
+	const std::chrono::duration<double> elapsed_seconds = end - start;
+
+	show_result("Decimal Form", subject.get_nat(), k, precision_digits, elapsed_seconds.count());
 
 	SUCCEED();
 }
